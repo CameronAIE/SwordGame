a907623b745a18585e10fc7ad7d545c19b4f72bd
Oskar <s200080@students.aie.edu.au>
2023-08-31 14:51:14 +0930

    Working on a pause menu
    Pause currently just freezes time without any menu

--- Assets/Oskar/GameManager.cs
+++ Assets/Oskar/GameManager.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject DebugMenu, PauseMenu;
+    [SerializeField]
+    private SphereManager SphereManager;
+    private bool DebugMode;
+
+    public enum GameState
+    {
+        Active,
+        Paused,
+        Killed
+    }
+    GameState gameState;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        DebugMode = false;
+        gameState = GameState.Active;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (gameState)
+        {
+            case GameState.Active:
+
+                
+                break;
+            case GameState.Paused:
+                
+                break;
+            case GameState.Killed:
+
+                break;
+        }
+    }
+
+    public void PauseGame()
+    {
+        switch (gameState)
+        {
+            case GameState.Paused:
+                Time.timeScale = 1;
+                gameState = GameState.Active;
+                break;
+            case GameState.Active:
+                Time.timeScale = 0;
+                gameState = GameState.Paused;
+                break;
+            default: break;
+        }
+    }
+
+    public void ToggleDebug()
+    {
+        DebugMode = !DebugMode;
+        DebugMenu.SetActive(DebugMode);
     }
 }

e3b65936a5ba7c0089bd9a33e2110f904df14cb1
AIE\s221308 <cameron18556@gmail.com>
2023-08-31 14:27:27 +0930

    added spawnpoint script

---
+++ Assets/Cameron/Scripts/Spawn.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject[] enemies;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnEnemy()
+    {
+        int choice = Random.Range(0, enemies.Length);
+        Instantiate(enemies[choice], transform.position, transform.rotation);
+    }
+}

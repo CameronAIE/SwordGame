5b381a17a1e849da37af0271b301ad05877a9032
Oskar <s200080@schools.aie.edu.au>
2023-08-31 13:49:45 +0930

    Added Mobile UI

---
+++ Assets/Oskar/Scripts/SphereManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SphereManager : MonoBehaviour
+{
+    [SerializeField]
+    private int Score, Health;
+    [SerializeField]
+    private TextMeshProUGUI healthText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Health = 100;
+        Score = 0;
+    }
+
+    /// <summary>
+    /// decreases health by dmg
+    /// </summary>
+    /// <param name="dmg"></param>
+    public void ObjDamage(int dmg)
+    {
+        Health -= dmg;
+        healthText.text = $"Health\n{Health} | 100";
+    }
+
+    /// <summary>
+    /// adds "score" to total score
+    /// </summary>
+    /// <param name="score"></param>
+    public void AddScore(int score)
+    {
+        Score += score;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if (other.CompareTag("Enemy")) { other.GetComponent<GameObject>().GetComponent<Damage>(); }
+        
+    }
+}

b54a8cde2bdb3dbeba72cd90dd8f217a7810a2c9
AIE\s220969 <s220969@students.aie.edu.au>
2023-09-01 11:07:21 +0930

    More sword Changes

---
+++ Assets/Aidan/Scripts/Sword.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    private HingeJoint hj;
+    private Rigidbody rb;
+    private MeshCollider col;
+    [SerializeField] private SphereManager SphereManager;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //gets required components
+        hj = transform.GetComponentInParent<HingeJoint>();
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<MeshCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //the position the sword is required to go
+        Vector3 swordPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
+  Input.mousePosition.y, Camera.main.nearClipPlane + 9));
+        //updates the current position of the sword
+
+        //Debug.Log(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
+  //Input.mousePosition.y, Camera.main.nearClipPlane)));
+        hj.connectedAnchor = swordPos;
+
+        //Debug.Log(hj.velocity);
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Enemy"))
+        {
+            collision.transform.GetComponent<Damage>().TakeDamage(1);
+        }
+        else if (collision.transform.CompareTag("Death"))
+        {
+            SphereManager.ObjDamage(collision.transform.GetComponent<Damage>().damage);
+            Debug.Log("base has taken damage!");
+        }
+    }
+
+
+}

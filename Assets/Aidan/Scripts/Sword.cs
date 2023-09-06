4d15bf1fe9919463f24cb07e24afa9555b8b4845
AIE\s220969 <s220969@students.aie.edu.au>
2023-09-06 16:44:27 +0930

    Rewrote powerup system, powerups toggleable for debug, added light powerup/effects

--- Assets/Aidan/Scripts/Sword.cs
+++ Assets/Aidan/Scripts/Sword.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class Sword : MonoBehaviour
 {
     private HingeJoint hj;
+    private Rigidbody rb;
     private Transform pivot;
-    private MeshCollider col;
-    [SerializeField] private SphereManager SphereManager;
+    [Header("Values")]
+    public bool[] powerups; //0 = laser
+                            //1 = lighter sword
     [SerializeField] private float smoothVal = 6f; //greater = faster smoothing
     [SerializeField] private float defaultSize = 1f; //The size the sword will rest to if no size modification is active
-    [SerializeField] ParticleSystem sparks;
+    [Header("Prefabs")]
     [SerializeField] GameObject laser;
+    [Header("Assigned objects")]
+    [SerializeField] private SphereManager SphereManager;
+    [SerializeField] ParticleSystem sparks;
+    [SerializeField] ParticleSystem laserFire;
+    [SerializeField] ParticleSystem lightEffect;
 
-    public bool[] powerups; //0 = laser
-                            //1 =...
+
+
+
+
     private float laserShoot = 0;
 
     private float sizeTimer = 0;
@@ -25,9 +34,10 @@ public class Sword : MonoBehaviour
     void Start()
     {
         //gets required components
+        rb = GetComponent<Rigidbody>();
         hj = transform.GetComponentInParent<HingeJoint>();
         pivot = transform.parent;
-        col = GetComponent<MeshCollider>();
+        //col = GetComponent<MeshCollider>();
     }
 
     // Update is called once per frame
@@ -57,24 +67,27 @@ public class Sword : MonoBehaviour
         ParticleSystem.EmissionModule emission = sparks.emission;
         emission.enabled = Mathf.Abs(hj.velocity) > 500;
 
-        Debug.Log(hj.angle);
+        //Debug.Log(hj.angle);
 
+        //if laser powerup is active
         if (powerups[0])
         {
+            //if timer has reached 0
             if (laserShoot <= 0)
             {
+                //creates laser and parents it (so the laser is centered properly)
                 Transform tip = transform.GetChild(0);
                 GameObject newLaser = Instantiate(laser, tip);
-
+                //unassigns the parent
                 newLaser.transform.parent = null;
-                
+                //sets a timer for the next shot
                 laserShoot = 0.15f;
             }
             else 
             {
+                //subtracts timer
                 laserShoot -= Time.deltaTime;
             }
-            //Instantiate(laser);
         }
 
         //Debug inputs for testing
@@ -85,6 +98,16 @@ public class Sword : MonoBehaviour
         if (Input.GetMouseButton(1))
         {
             GrowSword(0.5f, 5f);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if (!powerups[0]) EnablePowerUp(0);
+            else DisablePowerUp(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            if (!powerups[1]) EnablePowerUp(1);
+            else DisablePowerUp(1);
         }
 
     }
@@ -102,9 +125,9 @@ public class Sword : MonoBehaviour
         }
         else if (collision.transform.CompareTag("Death"))
         {
-            //Deals damage to base 
+            //Deals damage to base if death enemy is hit
 
-            //SphereManager.ObjDamage(collision.transform.GetComponent<Damage>().damage);
+            SphereManager.ObjDamage(2);
             Debug.Log("base has taken damage!");
         }
     }
@@ -118,6 +141,55 @@ public class Sword : MonoBehaviour
     {
         sizeVal = size;
         sizeTimer = time;
+    }
+
+    /// <summary>
+    /// Enables a powerup based on the index provided. List of powerups is next to the powerups[] bool;
+    /// </summary>
+    public void EnablePowerUp(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                ParticleSystem.EmissionModule fireEmission = laserFire.emission;
+                fireEmission.enabled = true;
+                powerups[0] = true;
+                break;
+            case 1:
+                ParticleSystem.EmissionModule lightEmission = lightEffect.emission;
+                lightEmission.enabled = true;
+                rb.mass = 400;
+                rb.drag = 0;
+                smoothVal = 10;
+                powerups[1] = true;
+                break;
+            default:
+                break;
+        }
+    }
+    /// <summary>
+    /// Disables a powerup based on the index provided. List of powerups is next to the powerups[] bool;
+    /// </summary>
+    public void DisablePowerUp(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                ParticleSystem.EmissionModule fireEmission = laserFire.emission;
+                fireEmission.enabled = false;
+                powerups[0] = false;
+                break;
+            case 1:
+                ParticleSystem.EmissionModule lightEmission = lightEffect.emission;
+                lightEmission.enabled = false;
+                rb.mass = 100;
+                rb.drag = 2;
+                smoothVal = 8;
+                powerups[1] = false;
+                break;
+            default:
+                break;
+        }
     }
 
 }

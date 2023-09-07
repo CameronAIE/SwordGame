a34eeaa0bf6bb723ca224e8092fe2e9a7568a497
AIE\s220969 <s220969@students.aie.edu.au>
2023-09-07 10:07:59 +0930

    Added spawn cap and updated sword in main scene

--- Assets/Oskar/GameManager.cs
+++ Assets/Oskar/GameManager.cs
@@ -13,6 +13,9 @@ public class GameManager : MonoBehaviour
     private float spawnTimer; //countdown value for spawning
     [SerializeField] private float difficulty; //maximum wait time between enemies
     [SerializeField] private Spawn[] spawnLocations; //locations of all of the spawn points
+    [SerializeField] private int maxEnemies = 15; //maximum allowed enemies currently active in scene
+    [SerializeField] List<GameObject> enemiesList; //list of all active enemies
+     
 
     public enum GameState
     {
@@ -35,8 +38,26 @@ public class GameManager : MonoBehaviour
         switch (gameState)
         {
             case GameState.Active:
-
                 
+                if (spawnTimer > difficulty && enemiesList.Count <= maxEnemies)
+                {
+                    spawnTimer = 0;
+                    enemiesList.Add(spawnLocations[Random.Range(0, spawnLocations.Length)].SpawnEnemy());
+                    difficulty -= 0.005f;
+                    Mathf.Clamp(spawnTimer, 0.2f, 100);
+                }
+
+                spawnTimer += Time.deltaTime;
+
+                foreach(GameObject enemy in enemiesList)
+                {
+                    if (!enemy)
+                    {
+                        enemiesList.Remove(enemy);
+                        break;
+                    }
+                }
+
                 break;
             case GameState.Paused:
                 
@@ -46,14 +67,7 @@ public class GameManager : MonoBehaviour
                 break;
         }
 
-        if(spawnTimer > difficulty)
-        {
-            spawnTimer = 0;
-            spawnLocations[Random.Range(0, spawnLocations.Length)].SpawnEnemy();
-            difficulty -= 0.01f;
-        }
-
-        spawnTimer += Time.deltaTime;
+        
 
     }
 

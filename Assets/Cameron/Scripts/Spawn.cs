8b4d97b6dda1547f931821d38e569faae5783ccc
AIE\s221308 <cameron18556@gmail.com>
2023-09-07 09:37:21 +0930

    timer on spawns added

--- Assets/Cameron/Scripts/Spawn.cs
+++ Assets/Cameron/Scripts/Spawn.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Spawn : MonoBehaviour
 {
     public GameObject[] enemies;
+    [SerializeField]
+    private float timer;
+    private float realTimer;
+    private int avalibileEnemys = 2;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,13 +18,22 @@ public class Spawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        realTimer += Time.deltaTime;
+        if (realTimer >= timer)
+        {
+            realTimer = Time.deltaTime;
+            avalibileEnemys += 2;
+            if (avalibileEnemys > enemies.Length)
+            {
+                avalibileEnemys = enemies.Length;
+            }
+        }
     }
 
     public void SpawnEnemy()
     {
         //spawn random enemy from array
-        int choice = Random.Range(0, enemies.Length);
+        int choice = Random.Range(0, avalibileEnemys);
         Instantiate(enemies[choice], transform.position, transform.rotation);
     }
 }

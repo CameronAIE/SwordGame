55850e3787da2e6572fe30c006eaef171e1733c8
AIE\s221308 <cameron18556@gmail.com>
2023-09-13 10:06:07 +0930

    added power uping

--- Assets/Oskar/GameManager.cs
+++ Assets/Oskar/GameManager.cs
@@ -9,13 +9,21 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private GameObject DebugMenu, PauseMenu;
     [SerializeField]
-    private SphereManager SphereManager;
+    private SphereManager sphereManager;
     private bool DebugMode;
+    [SerializeField] private Sword player;
     private float spawnTimer; //countdown value for spawning
     [SerializeField] private float difficulty; //maximum wait time between enemies
     [SerializeField] private Spawn[] spawnLocations; //locations of all of the spawn points
     [SerializeField] private int maxEnemies = 15; //maximum allowed enemies currently active in scene
     [SerializeField] List<GameObject> enemiesList; //list of all active enemies
+    [SerializeField] private int chance; // chance of a power up 0 is one in one 100 is one in 101
+    [SerializeField] private int powerUpDamage;
+    [SerializeField] private float pushBack;
+    [SerializeField] private float swordSize;
+    [SerializeField] private float bigSwordTime;
+    [SerializeField] private int powerUpHeal;
+    [SerializeField] private CameraZoom cameraZoom;
      
 
     public enum GameState
@@ -56,10 +64,14 @@ public class GameManager : MonoBehaviour
                     if (!enemy)
                     {
                         enemiesList.Remove(enemy);
+                        if(Random.Range(0, chance) == 0)
+                        {
+                            PowerUp();
+                        }
                         break;
                     }
                 }
-                if (SphereManager.IsDead())
+                if (sphereManager.IsDead())
                 {
                     gameState = GameState.Killed;
                 }
@@ -75,14 +87,14 @@ public class GameManager : MonoBehaviour
                 }
                 break;
             case GameState.Killed:
-
+                Dying();
                 break;
         }
     }
 
     public void QuitToMenu()
     {
-        SphereManager.SaveScore();
+        sphereManager.SaveScore();
         SceneManager.LoadScene("MainMenu");
     }
 
@@ -108,5 +120,50 @@ public class GameManager : MonoBehaviour
     {
         DebugMode = !DebugMode;
         DebugMenu.SetActive(DebugMode);
+    }
+
+    private void Dying()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    private void PowerUp()
+    {
+        int result = Random.Range(1, 9);
+
+        switch (result)
+        {
+            case 1:
+                
+                player.EnablePowerUp(1);
+                break;
+            case 2:
+                player.EnablePowerUp(0); ;
+                break;
+            case 3:
+                foreach (GameObject enemy in enemiesList)
+                {
+                    enemy.GetComponent<Damage>().Die(true);
+                }
+                break;
+            case 4:
+                sphereManager.ObjDamage(powerUpDamage);
+                break;
+            case 5:
+                foreach (GameObject enemy in enemiesList)
+                {
+                    enemy.GetComponent<Damage>().PushBack(pushBack);
+                }
+                break;
+            case 6:
+                player.GrowSword(swordSize, bigSwordTime);
+                break;
+            case 7:
+                sphereManager.ObjDamage(powerUpHeal);
+                break;
+            case 8:
+                cameraZoom.Zoom();
+                break;
+        }
     }
 }

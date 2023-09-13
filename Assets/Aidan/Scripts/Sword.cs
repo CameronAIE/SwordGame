529a70fbc9d79e1c3cfbfc015e25a3138bd33b0d
AIE\s221308 <cameron18556@gmail.com>
2023-09-13 11:09:25 +0930

    the sword had a line preventing the creation of builds

--- Assets/Aidan/Scripts/Sword.cs
+++ Assets/Aidan/Scripts/Sword.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Transactions;
 using Unity.VisualScripting;
-using UnityEditor.Build;
 using UnityEngine;
 
 public class Sword : MonoBehaviour
